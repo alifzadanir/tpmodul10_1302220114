8eb1c5dd2d80b775a70775cdb3d6172f0d4c2b4c
alifzadanir <alifadanir@gmail.com>
2024-04-28 13:53:22 +0700

    first commit

---
+++ tpmodul10_1302220114/mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿﻿namespace tpmodul10_1302220114
+{
+    public class Mahasiswa
+    {
+        public string Nama {get; set;}
+        public string NIM { get; set; }
+
+        public Mahasiswa(string Nama, string NIM)
+        {
+            this.Nama = Nama;
+            this.NIM = NIM;
+        }
+    }
+}
