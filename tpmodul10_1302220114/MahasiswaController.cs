3b79775103a3b1899ded281cf6f96dbb232b1b2e
alifzadanir <alifadanir@gmail.com>
2024-04-28 14:03:23 +0700

    Menambahkan API Mahasiswa

---
+++ tpmodul10_1302220114/MahasiswaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul10_1302220114.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa("Alif Adani Rahmat","1302220114"),
+            new Mahasiswa("Nizar Arifin","1302220035"),
+            new Mahasiswa("Muhammad Yudha Pratama", "1302223117"),
+            new Mahasiswa("Muhammad Rizqi Haqqi", "1302220016"),
+            new Mahasiswa("Keenawa Wira Tutu", "1302223111")
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswaList;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mahasiswaList[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mahasiswa)
+        {
+            mahasiswaList.Add(mahasiswa);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            mahasiswaList.RemoveAt(id);
+        }
+
+    }
+}
